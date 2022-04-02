589e9dfdf1c614b71a83be8caeabb2d9cf0c8d81
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-04-02 19:05:14 +0600

    mockAggregator added

--- FundMe/scripts/deploy.py
+++ FundMe/scripts/deploy.py
@@ -1,11 +1,26 @@
 from os import access
-from brownie import FundMe, accounts
+from brownie import FundMe, MockV3Aggregator, accounts, network, config
 from scripts.helpful_scripts import getAccount
 
 
 def deploy_fund_me():
 
     account = getAccount()
+    # pass the price feed address to our fundme contract
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+
+    else:
+        print(f"The active network is {network.show_active()}")
+        print(f"Deploying Mocks...")
+        mock_aggregator = MockV3Aggregator.deploy(
+            18, 2000000000000000000000, {"from": account}
+        )
+        price_feed_address = mock_aggregator.address
+        print("Mocks deployed!")
+
     fund_me = FundMe.deploy({"from": account}, publish_source=True)
     print(f"Contract deployed to {fund_me.address}")
 
