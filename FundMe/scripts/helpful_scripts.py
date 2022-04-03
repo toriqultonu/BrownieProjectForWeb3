79ba8b181611673e436ba79e5ec972d4f433ffd9
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-04-03 10:13:02 +0600

    deploy mocks created

--- FundMe/scripts/helpful_scripts.py
+++ FundMe/scripts/helpful_scripts.py
@@ -1,4 +1,6 @@
-from brownie import accounts, config, FundMe, network
+import imp
+from brownie import accounts, config, FundMe, network, MockV3Aggregator
+from web3 import Web3
 
 
 def getAccount():
@@ -8,3 +10,11 @@ def getAccount():
 
     else:
         return accounts.add(config["wallets"]["from_key"])
+
+
+def deploy_mocks():
+    print(f"The active network is {network.show_active()}")
+    print(f"Deploying Mocks...")
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(18, Web3.toWei(2000, "ether"), {"from": getAccount()})
+    print("Mocks deployed!")
