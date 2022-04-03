c3e7b134c63ce5fc3c0fc3f7d77b9c54a5a21c9e
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-04-03 10:12:35 +0600

    refactored

--- FundMe/scripts/deploy.py
+++ FundMe/scripts/deploy.py
@@ -1,6 +1,7 @@
 from os import access
 from brownie import FundMe, MockV3Aggregator, accounts, network, config
-from scripts.helpful_scripts import getAccount
+from scripts.helpful_scripts import getAccount, deploy_mocks
+from web3 import Web3
 
 
 def deploy_fund_me():
@@ -13,13 +14,8 @@ def deploy_fund_me():
         ]
 
     else:
-        print(f"The active network is {network.show_active()}")
-        print(f"Deploying Mocks...")
-        mock_aggregator = MockV3Aggregator.deploy(
-            18, 2000000000000000000000, {"from": account}
-        )
-        price_feed_address = mock_aggregator.address
-        print("Mocks deployed!")
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
 
     fund_me = FundMe.deploy(
         price_feed_address,
