d53ea47f7a98e9f55ce9b8c29c219b4a3ca65b9f
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-04-03 12:45:00 +0600

    getting entrance fee

--- FundMe/scripts/fund_and_withdraw.py
+++ FundMe/scripts/fund_and_withdraw.py
@@ -5,3 +5,9 @@ from scripts.helpful_scripts import getAccount
 def fund():
     fund_me = FundMe[-1]
     account = getAccount()
+    entrance_fee = fund_me.getEntranceFee()
+    print(entrance_fee)
+
+
+def main():
+    fund()
