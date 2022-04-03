c0987fee2f36d2c7b9b7210a683768ae5827d2ed
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-04-03 13:07:10 +0600

    just messing

--- FundMe/scripts/helpful_scripts.py
+++ FundMe/scripts/helpful_scripts.py
@@ -4,8 +4,8 @@ from web3 import Web3
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
-DECIMALS = 18
-STARTING_PRICE = 2000
+DECIMALS = 8
+STARTING_PRICE = 200000000000
 
 
 def getAccount():
