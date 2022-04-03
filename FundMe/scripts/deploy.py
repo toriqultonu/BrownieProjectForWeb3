09f1e07498d2f0a981480651413456754cc8745e
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2022-04-03 10:46:07 +0600

    refactored

--- FundMe/scripts/deploy.py
+++ FundMe/scripts/deploy.py
@@ -1,6 +1,10 @@
 from os import access
 from brownie import FundMe, MockV3Aggregator, accounts, network, config
-from scripts.helpful_scripts import getAccount, deploy_mocks
+from scripts.helpful_scripts import (
+    getAccount,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
 from web3 import Web3
 
 
@@ -8,7 +12,7 @@ def deploy_fund_me():
 
     account = getAccount()
     # pass the price feed address to our fundme contract
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]
